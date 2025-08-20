32c460df87f31e0a7794c1f38dc2b05eb90b6688
ARTHUR ANGELUS ANDRADE DE ALMEIDA <25132783@CFP127ALUNO.LOCAL>
2025-08-20 11:27:02 -0300

    finalizando atividade de funções

---
+++ funcoes.js
@@ -0,0 +1,11 @@
+function calcularSoma(){
+    let v1 = parseInt(document.getElementById("n1").value);
+    let v2 = parseInt(document.getElementById("n1").value);
+
+    let total = v1 + v2
+    document.getElementById('resultado').innerHTML = total;
+}
+
+function emitirNota(){
+    fetch()
+}
